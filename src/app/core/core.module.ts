db5591803ab96051d5c6a44439e51b5c1095343c
murage <murage@labskenya.com>
2022-02-20 12:44:33 +0300

    fix: cannot use object before declaration.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,13 @@ import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core
 import { HttpService } from './services/http.service';
 import { MetaService } from './services/meta.service';
 
+abstract class EnsureImportedOnce<T> {
+  constructor(targetModule: any) {
+    if (targetModule) {
+      throw new Error(`You are trying to import ${targetModule.constructor.name} again`)
+    }
+  }
+}
 
 @NgModule({
   declarations: [],
@@ -16,14 +23,6 @@ export class CoreModule extends EnsureImportedOnce<CoreModule> {
     return {
       ngModule: CoreModule,
       providers: [HttpService, MetaService] //singleton services that shouldn't be recreated
-    }
-  }
-}
-
-abstract class EnsureImportedOnce<T> {
-  constructor(targetModule: T) {
-    if (targetModule) {
-      throw new Error(`You are trying to import ${targetModule.constructor.name} again`)
     }
   }
 }
