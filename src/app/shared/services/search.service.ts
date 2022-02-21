b660b1a06ea645d9832fe058c98cf6df13b7c56c
murage <murage@labskenya.com>
2022-02-21 06:49:31 +0300

    feat: add search model service

---
+++ src/app/shared/services/search.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { map, Observable, Subject } from 'rxjs';
+import { SharedModule } from '../shared.module';
+import { HttpService } from '../../core/services/http.service';
+
+@Injectable({providedIn: SharedModule})
+export class SearchService<T> {
+
+  private searchFields: (keyof T | string)[] = [];
+
+  constructor(private http: HttpService) {
+  }
+
+  setFields(fields: (keyof T | string)[]) {
+    this.searchFields = this.searchFields.concat(fields);
+  }
+
+  find(searchTerm: string | number, model?: Subject<T[]> | null, route?: string): Observable<T[]> {
+    if (this.searchFields.length == 0) {
+      throw new Error('Search fields not provided')
+    }
+
+    let hits: Observable<T[]> = new Observable<T[]>();
+
+    //if there is a route, search the backend
+    if (route) {
+      hits = this.http.get(route, {withoutToken: true, params: {search: searchTerm},});
+    } else if (model) {
+      hits = model.pipe(map((rows: T[], index) => {
+
+        return rows.filter((row: any) => {
+          return this.searchFields
+            .some((field) => this.hasHit(String(searchTerm).toLowerCase(), row, field));
+        });
+      }));
+    }
+
+    return hits;
+  }
+
+  hasHit(searchTerm: string, dataRow: any[], field: any): boolean {
+    const keyDelimiter = '.';
+    let nestedKey: string[] = [];
+    const isNested = (field as string).includes(keyDelimiter);
+
+
+    if (isNested) {
+      nestedKey = (field as string).split(keyDelimiter);
+      field = nestedKey.shift();//remove the first field key
+    }
+
+    //if field is not provided or no field in the row
+    if (!field || !dataRow[ field ]) {
+      return false
+    }
+
+
+    if (isNested) {
+
+      if (typeof dataRow[ field ] === 'object' && keyDelimiter.length > 0) {
+        //recursive call
+        return this.hasHit(searchTerm, dataRow[ field ], nestedKey.join(keyDelimiter))
+      } else if (Array.isArray(dataRow[ field ])) {
+        return (dataRow[ field ]).some((v: any) => String(v).toLowerCase().includes(searchTerm));
+      }
+      //it's safe to assume this is impossible
+      return false;
+    }
+    if (typeof dataRow[ field ] === 'string' || typeof dataRow[ field ] === 'boolean'
+      || typeof dataRow[ field ] === 'number' || typeof dataRow[ field ] === 'bigint') {
+
+      return String(dataRow[ field ]).toLowerCase().includes(searchTerm)
+    }
+
+    return false;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentHeaderComponent } from './components/content-header/content-header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchService } from './services/search.service';
 
 @NgModule({
   declarations: [
     ContentHeaderComponent
   ],
   imports: [
-    CommonModule],
-  exports:[
-    ContentHeaderComponent
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    ContentHeaderComponent,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [SearchService]
 })
 export class SharedModule {}
