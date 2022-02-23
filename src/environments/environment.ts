5403f36c41fef477d308132ad58c477d34242a12
murage <murage@labskenya.com>
2022-02-23 11:29:08 +0300

    fix: on navigation, scroll to top

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   app: {
     name: 'PPA APP',
-    logoUrl: '',
+    logoUrl: 'https://mediantinternational.com/assets/images/main-logo.png',
     apiUrl: '',
     faviconUrl: 'favicon.ico'
   }
