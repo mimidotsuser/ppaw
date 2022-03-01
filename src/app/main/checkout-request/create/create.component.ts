625d4d277ce2f1bb3b1975c5c2d78ea3fce4c4a8
murage <murage@labskenya.com>
2022-03-01 15:22:38 +0300

    fix: checkout request create form

--- src/app/main/checkout-request/create/create.component.ts
+++ src/app/main/checkout-request/create/create.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable, of, startWith, Subscription, switchMap, tap } from 'rxjs';
+import {
+  map,
+  Observable,
+  Observer,
+  of,
+  startWith,
+  Subject,
+  Subscription,
+  switchMap,
+  tap
+} from 'rxjs';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { MRFPurpose } from '../../../models/m-r-f.model';
 import { WorksheetModel } from '../../../models/worksheet.model';
@@ -16,21 +26,22 @@ import { ProductModel } from '../../../models/product.model';
 })
 export class CreateComponent implements OnInit, OnDestroy {
 
-  itemsSearchInput = new FormControl();
+  searchInput = new FormControl();
   form: FormGroup;
   showPopupForm = false;
   itemType = {machine: 'Machine', spare: 'Spare'}
   remarksControl = new FormControl('', {validators: Validators.required});
   faEllipsisV = faEllipsisV;
   subscriptions: Subscription[] = [];
-  _selectedItems: FormModel[] = [];
-  private _selectedItems$: Observable<FormModel[]> = new Observable<FormModel[]>();
 
+  private formModelOnEdit: FormModel | null = null;
+  private formModelItems: FormModel[] = [];
+  private filteredItems$: Observable<FormModel[]> = new Observable<FormModel[]>();
+  private searchFields = ['type', 'parent.local_description', 'parent.item_code',
+    'product.local_description', 'product.item_code', 'purpose', 'qty', 'client.name',
+    'client.branch', 'client.region'];
 
   constructor(private fb: FormBuilder, private searchService: SearchService<FormModel>) {
-    this.searchService.setFields(['type', 'parent.local_description', 'parent.item_code',
-      'product.local_description', 'product.item_code', 'purpose', 'qty', 'client.name',
-      'client.branch', 'client.region']);
     this.form = this.fb.group({
       type: new FormControl(this.itemType.machine, {validators: [Validators.required]}),
       parent: new FormControl(),
@@ -73,11 +84,16 @@ export class CreateComponent implements OnInit, OnDestroy {
       this.subscriptions.push(y);
     }
 
+
     //search
-    this._selectedItems$ = this.itemsSearchInput.valueChanges.pipe(
-      startWith(''),
-      switchMap((v) => this.searchService.find(v, of(this._selectedItems)))
-    )
+    this.filteredItems$ = this.searchInput.valueChanges
+      .pipe(startWith(''))
+      .pipe(map((searchTerm: string) => {
+        return this.formModelItems.filter((row: any) => {
+          return this.searchFields
+            .some((field: string) => this.searchService.hasHit(searchTerm.toLowerCase(), row, field))
+        })
+      }));
   }
 
   get purposes(): { id: MRFPurpose, title: string }[] {
@@ -99,36 +115,60 @@ export class CreateComponent implements OnInit, OnDestroy {
   }
 
   closePopup() {
+    if (this.formModelOnEdit) {
+      this.formModelItems.push(this.formModelOnEdit);
+    }
     this.showPopupForm = false;
   }
 
   showCreateForm(model: FormModel | null = null) {
     if (model) {
       this.form.patchValue(model);
+      this.formModelOnEdit = model;
+      this.removeSelectedItem(model);
+    } else {
+      this.formModelOnEdit = null;
+      this.form.reset({type: this.itemType.machine});
     }
     this.showPopupForm = true;
   }
 
   get selectedItems(): Observable<FormModel[]> {
-    return this._selectedItems$;
+    return this.searchInput.value ? this.filteredItems$ : of(this.formModelItems);
   }
 
 
   removeSelectedItem(model: FormModel) {
-    const index = this._selectedItems.findIndex((item) => {
+    const index = this.formModelItems.findIndex((item) => {
       return item.purpose === model.purpose && item.product.id === model.product.id
         && item.client.id === model.client.id;
     });
     if (index > -1) {
-      this._selectedItems.splice(index, 1);
+      this.formModelItems.splice(index, 1);
     }
   }
 
   addItem() {
     this.form.markAllAsTouched();
-    if (this.form.valid) {
-      this._selectedItems.push(this.form.value);
+    if (!this.form.valid) {
+      return
     }
+    //check if it already exists
+    const item = this.form.value as FormModel;
+    const index = this.formModelItems.findIndex((model) => {
+      return model.client.id === item.client.id && model.purpose === item.purpose
+        && model.product.id === item.product.id && model.parent?.id === item.parent?.id;
+    });
+
+    if (index > -1) {
+      //only update the qty
+      this.formModelItems[ index ].qty += item.qty;
+    } else {
+      this.formModelItems.push(item);
+    }
+
+    this.closePopup()
+
   }
 
   submit() {
