bf68c39d165ded6ec95b437040848caa6ceeb04e
murage <murage@labskenya.com>
2022-03-07 12:15:36 +0300

    feat: add new product checkin modul

---
+++ src/app/main/checkin/purchased-products/create/create.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CreateComponent } from './create.component';
+import { SharedModule } from '../../../../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    CreateComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{path: '', component: CreateComponent}]),
+    SharedModule,
+  ]
+})
+export class CreateModule {}
--- src/app/main/checkin/purchased-products/purchased-products.module.ts
+++ src/app/main/checkin/purchased-products/purchased-products.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { SharedModule } from '../../../shared/shared.module';
 import { PurchasedProductsComponent } from './purchased-products.component';
 
 
@@ -11,7 +11,16 @@ import { PurchasedProductsComponent } from './purchased-products.component';
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{path: '', component: PurchasedProductsComponent}])
+    RouterModule.forChild([
+      {
+        path: '', component: PurchasedProductsComponent, children: [
+          {
+            path: 'create',
+            loadChildren: () => import('./create/create.module').then(m => m.CreateModule)
+          }
+        ]
+      },
+    ]),
   ]
 })
 export class PurchasedProductsModule {}
