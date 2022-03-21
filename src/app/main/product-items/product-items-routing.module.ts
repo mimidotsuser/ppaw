57d0904f688a7fc8bdb0ac0f107bd6d0b4ab1b36
murage <murage@labskenya.com>
2022-03-21 17:16:53 +0300

    feat: add product items index module

--- src/app/main/product-items/product-items-routing.module.ts
+++ src/app/main/product-items/product-items-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductItemsComponent } from './product-items.component';
 import { Actions, Resources } from '../../utils/permissions';
 
-const routes: Routes = [{
-  path: '', component: ProductItemsComponent,
-  data: {resource: Resources.productItems, action: Actions.view}
-}];
+const routes: Routes = [
+  {
+    path: '', component: ProductItemsComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./index/index.module').then(m => m.IndexModule),
+        data: {resource: Resources.productItems, action: Actions.view}
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
