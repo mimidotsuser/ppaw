b0d53c8af0d231c837cfdde0f57448de9ca4ee8d
murage <murage@labskenya.com>
2022-04-20 13:22:39 +0300

    refactor: main ui menu items into a getter

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -44,202 +44,6 @@ export class MainComponent implements OnInit, OnDestroy {
   totalDownloadJobs = 0;
   private _subscriptions: Subscription[] = [];
 
-  menuList: { [ key: string ]: MenuItem } = {
-    dashboard: {
-      title: 'Dashboard',
-      display: true,
-      icon: faHome,
-      items: [
-        {title: 'Dashboard', url: 'home', display: true}
-      ]
-    },
-    inventory_requisition: {
-      title: 'Material Requisition',
-      display: true,
-      icon: faWarehouse,
-      items: [
-        {title: 'New request', url: 'material-requisition/create', display: true},
-        {title: 'All requests', url: 'material-requisition/history', display: true, exact: false},
-        {
-          title: 'Verify requests', url: 'material-requisition/verification',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Approve requests',
-          url: 'material-requisition/approval',
-          display: true,
-          exact: false
-        },
-      ]
-    },
-    checkout: {
-      title: 'Checkout',
-      display: true,
-      icon: faPeopleCarry,
-      items: [
-        {title: 'Checkout Requests', url: 'checkout/issue-requests', display: true, exact: false},
-      ]
-    },
-    purchase_requests: {
-      title: 'Purchase Requests',
-      display: true,
-      icon: faShoppingBasket,
-      items: [
-        {title: 'New Requests', url: 'purchase-requisition/create', display: true},
-        {
-          title: 'All Purchase Requests',
-          url: 'purchase-requisition/history',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Requests Checking',
-          url: 'purchase-requisition/check',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Requests Approval', url: 'purchase-requisition/approve',
-          display: true,
-          exact: false
-        },
-      ]
-    },
-    rfq: {
-      title: 'RFQ',
-      display: true,
-      icon: faFileInvoiceDollar,
-      items: [
-        {title: 'New RFQ', url: 'request-for-quotations/create', display: true, exact: false},
-        {title: 'All RFQ\'s', url: 'request-for-quotations/history', display: true, exact: false},
-      ]
-    },
-    lpo: {
-      title: 'Purchase Orders',
-      display: true,
-      icon: faShoppingCart,
-      items: [
-        {title: 'New Purchase Order', url: 'purchase-orders/create', display: true, exact: false},
-        {title: 'All Purchase Orders', url: 'purchase-orders/history', display: true, exact: false},
-      ]
-    },
-    checkin: {
-      title: 'Products Checkin',
-      display: true,
-      icon: faDolly,
-      items: [
-        {title: 'Purchased Products', url: 'goods-receipt-note/create', display: true},
-        {
-          title: 'Spare Standby Reminder',
-          url: 'standby-spare-checkin',
-          display: true,
-          exact: false
-        },
-      ]
-    },
-    inspection: {
-      title: 'Products Inspection',
-      display: true,
-      icon: faCheckDouble,
-      items: [
-        {
-          title: 'Inspection Requests', url: 'inspection-note/purchased-products',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Inspection History', url: 'inspection-note/history',
-          display: true,
-          exact: false
-        }]
-    },
-    post_inspection: {
-      title: 'Post Inspection',
-      icon: faClipboardCheck,
-      display: true,
-      items: [
-        {
-          title: 'GRN/RGA Approval', url: 'goods-receipt-note/approval',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'All GRN/RGA Docs', url: 'goods-receipt-note/history',
-          display: true,
-          exact: false
-        }]
-    },
-    stock_balance: {
-      title: 'Tracking & Balances',
-      display: true,
-      icon: faCubes,
-      items: [
-        {
-          title: 'Product Tracking',
-          url: 'product-items',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Stock Balances', url: 'stock-balances',
-          display: true
-        },
-      ]
-    },
-    worksheets: {
-      title: 'Worksheets',
-      display: true,
-      icon: faBook,
-      items: [
-        {
-          title: 'New Worksheet', url: 'worksheets/create',
-          display: true
-        }, {
-          title: 'All Worksheets', url: 'worksheets/history',
-          display: true, exact: false
-        },
-      ]
-    },
-    customers: {
-      title: 'Clients & Contracts',
-      display: true,
-      icon: faAddressBook,
-      items: [
-        {
-          title: 'All Customers', url: 'customers',
-          display: true
-        },
-        {
-          title: 'All Contracts', url: 'customer-contracts',
-          display: true,
-          exact: false
-        },
-      ]
-    },
-    adminstration: {
-      title: 'Administration',
-      display: true,
-      icon: faUserShield,
-      items: [
-        {
-          title: 'Inventory Products',
-          url: 'products',
-          display: true,
-          exact: false
-        },
-        {
-          title: 'Staff Accounts', url: 'users',
-          display: true
-        },
-        {
-          title: 'User Roles', url: 'roles',
-          display: true
-        },
-      ]
-    }
-
-  }
 
   pageTitle: string = '';
   private _breadcrumbList: { label: string, title: string, path: string, active: boolean }[] = [];
@@ -278,7 +82,7 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   get menuBlocks(): string[] {
-    return Object.keys(this.menuList);
+    return this.menuList ? Object.keys(this.menuList) : [];
   }
 
   get name(): string {
@@ -286,6 +90,214 @@ export class MainComponent implements OnInit, OnDestroy {
       `${this.authService.user?.first_name} ${this.authService.user?.last_name}`
   }
 
+  get menuList(): { [ key: string ]: MenuList } {
+    return {
+      dashboard: {
+        title: 'Dashboard',
+        display: true,
+        icon: faHome,
+        items: [
+          {title: 'Dashboard', url: 'home', display: true}
+        ]
+      },
+      inventory_requisition: {
+        title: 'Material Requisition',
+        display: true,
+        icon: faWarehouse,
+        items: [
+          {title: 'New request', url: 'material-requisition/create', display: true},
+          {title: 'All requests', url: 'material-requisition/history', display: true, exact: false},
+          {
+            title: 'Verify requests', url: 'material-requisition/verification',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Approve requests',
+            url: 'material-requisition/approval',
+            display: true,
+            exact: false
+          },
+        ]
+      },
+      checkout: {
+        title: 'Checkout',
+        display: true,
+        icon: faPeopleCarry,
+        items: [
+          {title: 'Checkout Requests', url: 'checkout/issue-requests', display: true, exact: false},
+        ]
+      },
+      purchase_requests: {
+        title: 'Purchase Requests',
+        display: true,
+        icon: faShoppingBasket,
+        items: [
+          {title: 'New Requests', url: 'purchase-requisition/create', display: true},
+          {
+            title: 'All Purchase Requests',
+            url: 'purchase-requisition/history',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Requests Checking',
+            url: 'purchase-requisition/check',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Requests Approval', url: 'purchase-requisition/approve',
+            display: true,
+            exact: false
+          },
+        ]
+      },
+      rfq: {
+        title: 'RFQ',
+        display: true,
+        icon: faFileInvoiceDollar,
+        items: [
+          {title: 'New RFQ', url: 'request-for-quotations/create', display: true, exact: false},
+          {title: 'All RFQ\'s', url: 'request-for-quotations/history', display: true, exact: false},
+        ]
+      },
+      lpo: {
+        title: 'Purchase Orders',
+        display: true,
+        icon: faShoppingCart,
+        items: [
+          {title: 'New Purchase Order', url: 'purchase-orders/create', display: true, exact: false},
+          {
+            title: 'All Purchase Orders',
+            url: 'purchase-orders/history',
+            display: true,
+            exact: false
+          },
+        ]
+      },
+      checkin: {
+        title: 'Products Checkin',
+        display: true,
+        icon: faDolly,
+        items: [
+          {title: 'Purchased Products', url: 'goods-receipt-note/create', display: true},
+          {
+            title: 'Spare Standby Reminder',
+            url: 'standby-spare-checkin',
+            display: true,
+            exact: false
+          },
+        ]
+      },
+      inspection: {
+        title: 'Products Inspection',
+        display: true,
+        icon: faCheckDouble,
+        items: [
+          {
+            title: 'Inspection Requests', url: 'inspection-note/purchased-products',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Inspection History', url: 'inspection-note/history',
+            display: true,
+            exact: false
+          }]
+      },
+      post_inspection: {
+        title: 'Post Inspection',
+        icon: faClipboardCheck,
+        display: true,
+        items: [
+          {
+            title: 'GRN/RGA Approval', url: 'goods-receipt-note/approval',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'All GRN/RGA Docs', url: 'goods-receipt-note/history',
+            display: true,
+            exact: false
+          }]
+      },
+      stock_balance: {
+        title: 'Tracking & Balances',
+        display: true,
+        icon: faCubes,
+        items: [
+          {
+            title: 'Product Tracking',
+            url: 'product-items',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Stock Balances', url: 'stock-balances',
+            display: true
+          },
+        ]
+      },
+      worksheets: {
+        title: 'Worksheets',
+        display: true,
+        icon: faBook,
+        items: [
+          {
+            title: 'New Worksheet', url: 'worksheets/create',
+            display: true
+          }, {
+            title: 'All Worksheets', url: 'worksheets/history',
+            display: true, exact: false
+          },
+        ]
+      },
+      customers: {
+        title: 'Clients & Contracts',
+        display: true,
+        icon: faAddressBook,
+        items: [
+          {
+            title: 'All Customers', url: 'customers',
+            display: true
+          },
+          {
+            title: 'All Contracts', url: 'customer-contracts',
+            display: true,
+            exact: false
+          },
+        ]
+      },
+      adminstration: {
+        title: 'Administration',
+        display: true,
+        icon: faUserShield,
+        items: [
+          {
+            title: 'Inventory Products',
+            url: 'products',
+            display: true,
+            exact: false
+          },
+          {
+            title: 'Staff Accounts', url: 'users',
+            display: true
+          },
+          {
+            title: 'User Roles', url: 'roles',
+            display: true
+          },
+        ]
+      }
+
+    }
+  }
+
+
+  menuItemTrackBy(index: number, item: MenuItem) {
+    return item.title;
+  }
 
   resolveBreadcrumb(evt: ActivationEnd | NavigationEnd) {
 
@@ -341,12 +353,24 @@ export class MainComponent implements OnInit, OnDestroy {
     this._subscriptions.map((sub) => sub.unsubscribe());
 
   }
+
+  log(menuListElement: MenuList) {
+    console.log(menuListElement)
+  }
 }
 
-interface MenuItem {
+interface MenuList {
   title: string;
   icon?: IconProp;
   display: boolean;
-  items?: { title: string, icon?: IconProp, url: string, display: boolean, exact?: boolean }[]
+  items: MenuItem[]
 
+}
+
+interface MenuItem {
+  title: string,
+  icon?: IconProp,
+  url: string,
+  display: boolean,
+  exact?: boolean
 }
